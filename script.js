3fd867651c6a9bff44f8ae10e68ad2ca7e5dc25b
Priyank Patel <patel23priyank@gmail.com>
2022-04-05 00:17:31 +0530

    remove sticky navbar

--- script.js
+++ script.js
@@ -1,18 +1,8 @@
 const mainBody = document.querySelector('.main');
-const navbar = document.querySelector('.navbar');
 const logo = document.querySelector('.logo');
 const themeSwitch = document.querySelector('.theme-switch');
 const menu = document.querySelector('.navbar .menu');
 const menuBtn = document.querySelector('.menu-btn');
-
-// sticky navbar
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 20) {
-    navbar.classList.add('sticky');
-  } else {
-    navbar.classList.remove('sticky');
-  }
-});
 
 // hide slide out menu when clicking logo
 logo.addEventListener('click', () => {
