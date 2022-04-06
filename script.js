957dd4e1fde438e2259c7110b5833f2c6711f70d
Priyank Patel <patel23priyank@gmail.com>
2022-04-07 00:11:36 +0530

    dark mode

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ const menu = document.querySelector('.navbar .menu');
 const menuBtn = document.querySelector('.menu-btn');
 const scrollUpBtn = document.querySelector('.scroll-up-btn');
 
+const LIGHT_THEME = 'light';
+const DARK_THEME = 'dark';
+const SUN_ICON = 'night';
+
 // hide slide out menu when clicking navbar links
 navbarLinks.forEach((item) => {
   item.addEventListener('click', () => {
@@ -37,6 +41,41 @@ menuBtn.addEventListener('click', () => {
 });
 
 // switch theme
+function setTheme(theme) {
+  localStorage.setItem('theme', theme);
+  document.documentElement.setAttribute('data-theme', theme);
+  if (theme === LIGHT_THEME) {
+    document.querySelector('.home').style.backgroundImage = "url('./images/banner.jpg')";
+  } else {
+    document.querySelector('.home').style.backgroundImage = "url('./images/banner-dark.jpg')";
+  }
+}
+
+// toggle sun/moon icon
 themeSwitch.addEventListener('click', () => {
-  themeSwitch.classList.toggle('night'); // change sun/moon icon
+  if (themeSwitch.classList.contains(SUN_ICON)) {
+    themeSwitch.classList.remove(SUN_ICON);
+    setTheme(LIGHT_THEME);
+  } else {
+    themeSwitch.classList.add(SUN_ICON);
+    setTheme(DARK_THEME);
+  }
 });
+
+//#region page load
+
+const currentTheme = localStorage.getItem('theme');
+
+// set theme on page load
+if (currentTheme === null) {
+  currentTheme = LIGHT_THEME;
+  localStorage.setItem('theme', currentTheme);
+}
+
+if (currentTheme === DARK_THEME) {
+  themeSwitch.classList.add(SUN_ICON);
+}
+
+setTheme(currentTheme);
+
+//#endregion
