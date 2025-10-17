9e676113ebcb506f57e145ee71f84fa28ea3c644
Priyank Patel <patel23priyank@gmail.com>
2025-10-17 01:34:49 +0100

    add themes

--- themes.js
+++ themes.js
@@ -12,5 +12,26 @@ const THEMES = [
     text: 'hsl(0, 0%, 90%)',
     textMuted: 'hsl(0, 0%, 45%)',
     accent: 'hsl(125, 80%, 60%)'
+  },
+  {
+    bg: 'hsl(160, 60%, 5%)',
+    shadow: 'hsl(160, 20%, 30%)',
+    text: 'hsl(0, 0%, 90%)',
+    textMuted: 'hsl(0, 0%, 45%)',
+    accent: 'hsl(20, 80%, 60%)'
+  },
+  {
+    bg: 'hsl(210, 60%, 97.5%)',
+    shadow: 'hsl(210, 30%, 75%)',
+    text: 'hsl(35, 30%, 10%)',
+    textMuted: 'hsl(35, 10%, 45%)',
+    accent: 'hsl(160, 70%, 45%)'
+  },
+  {
+    bg: 'hsl(45, 100%, 97.5%)',
+    shadow: 'hsl(45, 40%, 75%)',
+    text: 'hsl(30, 30%, 10%)',
+    textMuted: 'hsl(30, 10%, 45%)',
+    accent: 'hsl(340, 80%, 55%)'
   }
 ];
